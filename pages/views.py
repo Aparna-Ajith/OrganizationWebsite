542b18bf082976fb68144a6d20d18fbb45aa258a
Lors Taeron <87148124+Aparna-Ajith@users.noreply.github.com>
2023-10-25 22:58:14 +0530

    login UI

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html", {})
+
+
+def login(request):
+    return render(request, "templates/registration/login.html", {})
--- website/settings.py
+++ website/settings.py
@@ -133,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 MEDIA_URL = "media/"
+
+LOGIN_REDIRECT_URL = "/"
