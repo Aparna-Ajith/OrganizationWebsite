961b0991827aec6552279b26c982fd8f0bc39f1f
Lors Taeron <87148124+Aparna-Ajith@users.noreply.github.com>
2023-10-29 16:13:03 +0530

    login and register function

--- website/settings.py
+++ website/settings.py
@@ -137,3 +137,6 @@ MEDIA_URL = "media/"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
