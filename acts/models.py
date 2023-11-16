12422aa4cfe1d1ffa339a3f8d8ca1b487d1fe9d2
Lors Taeron <87148124+Aparna-Ajith@users.noreply.github.com>
2023-11-16 21:07:39 +0530

    idk

--- acts/models.py
+++ acts/models.py
@@ -7,4 +7,4 @@ class Acts(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FileField(upload_to="acts_images/", blank=True)
+    image = models.FileField(upload_to="", blank=True)
