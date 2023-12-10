c53877b2754b9e22f26a3d135584877e9a6c5661
Lors Taeron <87148124+Aparna-Ajith@users.noreply.github.com>
2023-12-10 21:41:57 +0530

    payments

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from payments import views
+from payments.views import (CreateCheckoutSessionView,
+                            SuccessView,
+                            CancelView,
+                            ProductLandingPageView,)
 
 urlpatterns = [
     # path("", views.acts_index, name="acts_index"),
+    path('cancel/', CancelView.as_view(), name='cancel'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('create-checkout-session/<pk>/',
+         CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('', ProductLandingPageView.as_view(), name='landing'),
 
 ]
--- payments/views.py
+++ payments/views.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.views import View
 from .models import Price
+from .models import Product
+
+from django.views.generic import TemplateView
 # Create your views here.
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -23,7 +26,31 @@ class CreateCheckoutSessionView(View):
                 },
             ],
             mode='payment',
-            success_url=domain + '/success/',
-            cancel_url=domain + '/cancel/',
+            success_url=domain + '/payments/success/',
+            cancel_url=domain + '/payments/cancel/',
+
         )
         return redirect(checkout_session.url)
+
+
+class SuccessView(TemplateView):
+    template_name = "success.html"
+
+
+class CancelView(TemplateView):
+    template_name = "cancel.html"
+
+
+class ProductLandingPageView(TemplateView):
+    template_name = "landing.html"
+
+    def get_context_data(self, **kwargs):
+        product = Product.objects.get(name="Basic Tier")
+        prices = Price.objects.filter(product=product)
+        context = super(ProductLandingPageView,
+                        self).get_context_data(**kwargs)
+        context.update({
+            "product": product,
+            "prices": prices
+        })
+        return context
